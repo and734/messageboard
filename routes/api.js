6e7e3b1cda6f8cf092598fc4bd5f85d56320ffa7
George Phalen <gphalen52@gmail.com>
2025-01-02 02:02:17 +0000

    Main Checklist:
    <!-- Please follow this checklist and put an x in each of the boxes, like this: [x]. It will ensure that our team takes your pull request seriously. -->
    
    - [ ] I have read [freeCodeCamp's contribution guidelines](https://contribute.freecodecamp.org).
    - [ ] My pull request has a [descriptive title](https://contribute.freecodecamp.org/#/how-to-open-a-pull-request?id=prepare-a-good-pr-title) (**not** a vague title like `Update index.md`)
    
    <!--If your pull request closes a GitHub issue, replace the XXXXX below with the issue number.-->
    
    Closes #XXXXX
    
    <!-- Feel free to add any additional description of changes below this line -->

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,14 @@
 'use strict';
 
+var expect=require('chai').expect;
+var MongoClient=require('mongodb').MongoClient;
+var ObjectId=require('mongodb').ObjectId;
+const htmlSanitize=require('sanitize-html');
+
 module.exports = function (app) {
-  
+
+ app.route('/api/threads/board')
+
   app.route('/api/threads/:board');
     
   app.route('/api/replies/:board');
